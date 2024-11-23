cf9af00af71f4596b93ca9c9e20317daf4c3416e
Maciej Kopeć <maciejkopec92@gmail.com>
2024-11-23 23:02:06 +0100

    ifs

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -1,9 +1,16 @@
+use std::cmp::Ordering;
+
 fn bigger(a: i32, b: i32) -> i32 {
     // TODO: Complete this function to return the bigger number!
     // If both numbers are equal, any of them can be returned.
     // Do not use:
     // - another function call
     // - additional variables
+    match a.cmp(&b) {
+        Ordering::Greater => a,
+        Ordering::Less => b,
+        Ordering::Equal => a,
+    }
 }
 
 fn main() {
